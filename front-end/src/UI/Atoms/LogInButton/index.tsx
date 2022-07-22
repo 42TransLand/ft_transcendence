485027ae1eaee3877143db8dd1e88d78952feb7d
LeeByungKyu <69182505+Lee-Byung-Kyu@users.noreply.github.com>
2022-07-22 11:52:03 +0900

    Update front-end/src/UI/Atoms/LogInButton/index.tsx
    Co-authored-by: Tensiya <40755203+T2SU@users.noreply.github.com>

--- front-end/src/UI/Atoms/LogInButton/index.tsx
+++ front-end/src/UI/Atoms/LogInButton/index.tsx
@@ -2,19 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 import Button from 'react-bootstrap/Button';
 
-const BlackBox = styled.div`
-  background-color: black;
-  width: 300px;
-  height: 80px;
-`;
-
 function LogInButton() {
   return (
-    <BlackBox>
-      <Button variant="light" size="lg">
-        LOGIN
-      </Button>{' '}
-    </BlackBox>
+    <Button variant="light" size="lg">
+      LOGIN
+    </Button>
   );
 }
 
