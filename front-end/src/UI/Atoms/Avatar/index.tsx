d83022c90b3bbf22fee19c7f3b6a8863529fe3a3
Lee-Byung-Kyu <leebang54@khu.ac.kr>
2022-07-22 15:09:35 +0900

    TL42-63 feat. Avatar 컴포넌트 추가
    Avatar 컴포넌트와 default로 들어갈 이미지를 추가했다.

---
+++ front-end/src/UI/Atoms/Avatar/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Image from 'react-bootstrap/Image';
+
+function Avatar() {
+  return <Image src="default_user_image.png" roundedCircle />;
+}
+
+export default Avatar;
