907d6ba7ef3e5ddedd3117a6e828e605b51adcd2
Jihoon Lee <brain2400@gmail.com>
2022-07-29 11:43:41 +0900

    [TL42-100] style: 채널 생성 컴포넌트 스타일링
    - Grid를 이용해서 채널 생성 컴포넌트의 뼈대를 만듬
    - Input에도 flushed variant를 적용함
    - Button의 크기도 수정함

--- front-end/src/UI/Molecules/CreateChannel/index.tsx
+++ front-end/src/UI/Molecules/CreateChannel/index.tsx
@@ -1,31 +1,41 @@
-import { ChatIcon } from '@chakra-ui/icons';
-import { HStack, VStack, Input, Text, Divider, Button } from '@chakra-ui/react';
 import React from 'react';
+import { ChatIcon } from '@chakra-ui/icons';
+import { Grid, GridItem, VStack, Input, Text, Button } from '@chakra-ui/react';
 
 function CreateChannel() {
   return (
-    <HStack>
-      <ChatIcon fontSize={40} />
-      <VStack>
-        <Text fontSize={10} color="gray">
-          채널 이름
-        </Text>
-        <Input />
-        <Divider />
-        <Text fontSize={10} color="gray">
-          최대 인원
-        </Text>
-        <Input />
-        <Divider />
-        <Text fontSize={10} color="gray">
-          입장 비밀번호
-        </Text>
-        <Input type="password" />
-        <Button colorScheme="gray" size="xs">
+    <Grid
+      h="100%"
+      w="100%"
+      templateRows="reapeat(4, 1fr)"
+      templateColumns="repeat(6, 1fr)"
+    >
+      <GridItem rowSpan={1} colSpan={1}>
+        <ChatIcon fontSize={40} />
+      </GridItem>
+      <GridItem rowSpan={3} colSpan={5}>
+        <VStack align="baseline">
+          <Text fontSize={10} color="gray">
+            채널 이름
+          </Text>
+          <Input variant="flushed" />
+          <Text fontSize={10} color="gray">
+            최대 인원
+          </Text>
+          <Input variant="flushed" />
+          <Text fontSize={10} color="gray">
+            입장 비밀번호
+          </Text>
+          <Input type="password" variant="flushed" />
+        </VStack>
+      </GridItem>
+      <GridItem rowSpan={1} colSpan={4} />
+      <GridItem rowSpan={1} colSpan={2}>
+        <Button colorScheme="gray" width="100%">
           채널 생성
         </Button>
-      </VStack>
-    </HStack>
+      </GridItem>
+    </Grid>
   );
 }
 
