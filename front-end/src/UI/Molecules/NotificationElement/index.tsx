5a5c814a244d0083d91abeeb9943686f25173f37
Jihoon Lee <brain2400@gmail.com>
2022-07-29 16:21:15 +0900

    [TL42-101] style: NotificationElement 스타일링

--- front-end/src/UI/Molecules/NotificationElement/index.tsx
+++ front-end/src/UI/Molecules/NotificationElement/index.tsx
@@ -7,30 +7,35 @@ function NotificationElement(props: { userName: string }) {
 
   return (
     <Grid
-      h="90px"
+      h="100px"
       w="100%"
       templateRows="repeat(3, 1fr)"
       templateColumns="repeat(10, 1fr)"
       bgColor="blue.200"
       borderWidth="1px"
       borderColor="black"
+      paddingX={3}
+      paddingY={2}
     >
       <GridItem rowSpan={3} colSpan={1} margin="auto">
         <Avatar name={userName} size="md" />
       </GridItem>
       <GridItem rowSpan={1} colSpan={1} />
-      <GridItem rowSpan={1} colSpan={8}>
-        <Text fontSize="lg">{userName}님께서</Text>
+      <GridItem rowSpan={2} colSpan={8}>
+        <Text fontSize="lg" lineHeight={1.25}>
+          {userName}님께서
+          <br />
+          친구신청을 보냈습니다
+        </Text>
       </GridItem>
-      <GridItem rowSpan={1} colSpan={1} />
-      <GridItem rowSpan={1} colSpan={8}>
-        <Text fontSize="lg">친구신청을 보냈습니다</Text>
+      <GridItem rowSpan={2} colSpan={1} />
+      <GridItem rowSpan={1} colSpan={4} flex="">
+        <Text fontSize="sm" paddingY={1.5}>
+          방금전
+        </Text>
       </GridItem>
-      <GridItem rowSpan={1} colSpan={1} />
+      <GridItem rowSpan={1} colSpan={2} />
       <GridItem rowSpan={1} colSpan={2}>
-        <Text fontSize="md">방금전</Text>
-      </GridItem>
-      <GridItem rowSpan={1} colSpan={6}>
         <AcceptanceButton />
       </GridItem>
     </Grid>
