99ae66a7d624c980ff65f126af194ca14a7c2c7b
Tensiya <40755203+T2SU@users.noreply.github.com>
2022-08-03 18:08:06 +0900

    [TL42-112] fix: 라우팅 모달에 기능 추가
     - 반응형을 위해 size값 수정.
     - closeOnOverlayClick 값을 받아서, 바깥을 눌러도 꺼질 여부 반영

--- front-end/src/UI/Templates/RoutedModal/index.tsx
+++ front-end/src/UI/Templates/RoutedModal/index.tsx
@@ -4,23 +4,29 @@ import { useNavigate } from 'react-router-dom';
 
 export default function RoutedModal({
   children,
+  closeOnOverlayClick,
 }: {
   children: React.ReactNode;
+  closeOnOverlayClick?: boolean;
 }) {
   const [isOpen, onClose] = React.useState(true);
   const navigate = useNavigate();
 
   return (
     <Modal
-      closeOnOverlayClick={false}
+      closeOnOverlayClick={closeOnOverlayClick}
       isCentered
       isOpen={isOpen}
       onClose={() => onClose(false)}
       onCloseComplete={(): void => navigate('/', { replace: true })}
-      size="6xl"
+      size={{ base: 'full', lg: '6xl' }}
     >
       <ModalOverlay />
       <ModalContent>{children}</ModalContent>
     </Modal>
   );
 }
+
+RoutedModal.defaultProps = {
+  closeOnOverlayClick: true,
+};
