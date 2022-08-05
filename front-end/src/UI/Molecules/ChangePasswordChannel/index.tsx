04671013a7ea3a2a621fbaf5d76be8be3f921b3a
Tensiya <40755203+T2SU@users.noreply.github.com>
2022-08-05 10:59:34 +0900

    [TL42-118] feat: 방의 입장 비밀번호 수정 창
    - 기존 CreateChannel에서 내용을 거의 복붙 후, 패스워드 입력 칸만 남김.
    - 채팅방에서, 방 제목 옆의 자물쇠 및 말풍선 아이콘을 누르면 입장
      비밀번호 수정 창이 열림.
    - * 아직 관리자만 해당 수정 창을 열 수 있게 구현되지는 않았음.

---
+++ front-end/src/UI/Molecules/ChangePasswordChannel/index.tsx
@@ -0,0 +1,107 @@
+/* eslint-disable react/no-unused-prop-types */
+import React from 'react';
+import { LockIcon } from '@chakra-ui/icons';
+import { Grid, GridItem, VStack, Input, Button, Text } from '@chakra-ui/react';
+import {
+  ErrorMessage,
+  Field,
+  Form,
+  Formik,
+  FormikHelpers,
+  FormikValues,
+} from 'formik';
+import * as Yup from 'yup';
+import WarningAlertDialog from '../../Atoms/WarningAlertDialog';
+
+const ChangePasswordChannelScheme = Yup.object().shape({
+  password: Yup.string().max(
+    100,
+    '입장 비밀번호는 100자를 초과할 수 없습니다.',
+  ),
+});
+
+function ChangePasswordChannel() {
+  const [error, setError] = React.useState({
+    headerMessage: '',
+    bodyMessage: '',
+  });
+  const onSubmitHandler = React.useCallback(
+    (values: FormikValues, actions: FormikHelpers<FormikValues>) => {
+      setTimeout(() => {
+        setError({
+          headerMessage: '비밀번호 변경 실패',
+          bodyMessage: '입장 비밀번호를 변경하는데에 실패했습니다.',
+        });
+        actions.setSubmitting(false);
+      }, 1000);
+    },
+    [],
+  );
+  const cancelRef = React.useRef(null); // TODO: 삭제하기
+
+  return (
+    <Formik
+      initialValues={{}}
+      onSubmit={onSubmitHandler}
+      validationSchema={ChangePasswordChannelScheme}
+    >
+      {(props) => (
+        <>
+          <Form>
+            <Grid
+              h="full"
+              w="full"
+              templateRows="reapeat(4, 1fr)"
+              templateColumns="repeat(6, 1fr)"
+            >
+              <GridItem rowSpan={1} colSpan={1}>
+                <LockIcon fontSize={40} />
+              </GridItem>
+              <GridItem rowSpan={2} colSpan={5}>
+                <VStack align="baseline">
+                  <Field
+                    as={Input}
+                    variant="flushed"
+                    name="password"
+                    type="password"
+                    placeholder="변경할 입장 패스워드"
+                    my="0"
+                  />
+                  <Text
+                    fontSize="xs"
+                    textColor="red.500"
+                    mt="0!important"
+                    pb="12px"
+                  >
+                    <ErrorMessage name="password" />
+                  </Text>
+                </VStack>
+              </GridItem>
+              <GridItem rowSpan={2} colSpan={4} />
+              <GridItem rowSpan={1} colSpan={2}>
+                <Button
+                  type="submit"
+                  colorScheme="gray"
+                  width="100%"
+                  // eslint-disable-next-line react/prop-types
+                  isLoading={props.isSubmitting}
+                >
+                  변경하기
+                </Button>
+              </GridItem>
+            </Grid>
+          </Form>
+          <WarningAlertDialog
+            isOpen={error.bodyMessage.length > 0}
+            onClose={() => setError({ headerMessage: '', bodyMessage: '' })}
+            cancelRef={cancelRef}
+            headerMessage={error.headerMessage}
+            bodyMessage={error.bodyMessage}
+          />
+        </>
+      )}
+    </Formik>
+  );
+}
+
+export default ChangePasswordChannel;
--- front-end/src/UI/Organisms/ChatHeader/index.tsx
+++ front-end/src/UI/Organisms/ChatHeader/index.tsx
@@ -11,20 +11,32 @@ import { LockIcon } from '@chakra-ui/icons';
 import { IoIosChatbubbles } from 'react-icons/io';
 import { FiSend } from 'react-icons/fi';
 import { useChat } from '../../../Hooks/useChat';
+import PopoverButton from '../PopoverButton';
+import ChangePasswordChannel from '../../Molecules/ChangePasswordChannel';
 
 export default function ChatHeader() {
   const [chat] = useChat();
   const iconType = React.useMemo(() => {
-    if (chat.chatInfo.roomType === 'public') return IoIosChatbubbles;
-    if (chat.chatInfo.roomType === 'protected') return LockIcon;
-    return FiSend;
+    if (chat.chatInfo.roomType === 'public')
+      return <Icon as={IoIosChatbubbles} boxSize="3em" />;
+    if (chat.chatInfo.roomType === 'protected')
+      return <Icon as={LockIcon} boxSize="3em" />;
+    return <Icon as={FiSend} boxSize="3em" />;
   }, [chat]);
 
   return (
     <HStack justifyContent="space-between">
       <HStack justifyContent="flex-start">
         <Square centerContent>
-          <Icon as={iconType} boxSize="2em" />
+          <PopoverButton
+            icon={iconType}
+            placement="bottom-end"
+            transparent
+            w="420px"
+            h="170px"
+          >
+            <ChangePasswordChannel />
+          </PopoverButton>
         </Square>
         <VStack textAlign="left">
           <Text
