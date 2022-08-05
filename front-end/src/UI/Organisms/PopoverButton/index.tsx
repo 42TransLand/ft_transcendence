4add5ef5f527194cf1c9d16325469036635a4a70
Tensiya <40755203+T2SU@users.noreply.github.com>
2022-08-05 10:55:18 +0900

    [TL42-118] feat: 스타일 기능을 받는 PopoverButton
    - 기존 icon을 범용적인 icon 타입을 받게 수정
    - iconSize, placement, w, h를 PopoverButton에서 받아 적용될 수 있게
      수정.
    - transparent boolean 값을 통해, 버튼 배경의 투명을 조정할 수 있음.

--- front-end/src/UI/Organisms/PopoverButton/index.tsx
+++ front-end/src/UI/Organisms/PopoverButton/index.tsx
@@ -6,25 +6,54 @@ import {
   PopoverArrow,
   IconButton,
   PopoverCloseButton,
+  IconButtonProps,
+  PopoverProps,
+  PopoverContentProps,
 } from '@chakra-ui/react';
 
 function PopoverButton(props: {
-  icon: React.ReactElement;
+  icon: IconButtonProps['icon'];
   children: React.ReactNode;
+  iconSize?: IconButtonProps['size'];
+  placement?: PopoverProps['placement'];
+  w?: PopoverContentProps['w'];
+  h?: PopoverContentProps['h'];
+  transparent?: boolean | undefined;
 }) {
-  const { icon: ButtonIcon, children } = props;
+  const {
+    icon: ButtonIcon,
+    children,
+    iconSize,
+    placement,
+    w,
+    h,
+    transparent,
+  } = props;
+  const transparentProps = transparent
+    ? {
+        backgroundColor: 'transparent',
+        _hover: { backgroundColor: 'transparent' },
+        _active: { backgroundColor: 'transparent' },
+      }
+    : {};
 
   return (
-    <Popover placement="left">
+    <Popover placement={placement}>
       <PopoverTrigger>
-        <IconButton size="sm" aria-label="PopoverTrigger" icon={ButtonIcon} />
+        <IconButton
+          size={iconSize}
+          aria-label="PopoverTrigger"
+          icon={ButtonIcon}
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          {...transparentProps}
+        />
       </PopoverTrigger>
       <PopoverContent
         paddingX={5}
         paddingTop={10}
         paddingBottom={5}
-        w="420px"
-        h="340px"
+        w={w}
+        h={h}
       >
         <PopoverArrow />
         <PopoverCloseButton />
@@ -35,3 +64,11 @@ function PopoverButton(props: {
 }
 
 export default PopoverButton;
+
+PopoverButton.defaultProps = {
+  iconSize: 'sm',
+  placement: 'left',
+  w: '420px',
+  h: '340px',
+  transparent: false,
+};
