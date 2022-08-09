00be7eb74826e65d71068ac708913c879b15b118
T2SU <40755203+T2SU@users.noreply.github.com>
2022-08-09 18:58:44 +0900

    [TL42-151] fix: 실제 요청을 보내도록 수정
    - 실제 axios로 요청을 보내도록 수정.
    - alert삭제. 대신 오류 발생 시, WarningAlertDialog를 통해 에러를 출력.
    - 추후 react-query 기능을 사용해서, 친구리스트 캐시를 모두
      비우는 동작을 수행해야 함.

--- front-end/src/UI/Molecules/SearchFriendResultElement/index.tsx
+++ front-end/src/UI/Molecules/SearchFriendResultElement/index.tsx
@@ -2,42 +2,56 @@ import React from 'react';
 import { Text, HStack, Avatar, Button, Icon } from '@chakra-ui/react';
 import { FaUserPlus } from 'react-icons/fa';
 import UserSearchResultProps from '../../../Props/UserSearchResultProps';
+import WarningAlertDialog from '../../Templates/WarningAlertDialog';
+import useAddFriend from '../../../Hooks/useAddFriend';
 
 function SearchFriendResultElement({
   id,
   nickname,
   profileIcon,
 }: UserSearchResultProps) {
-  // TODO
-  const onAddFriend = React.useCallback(() => {
-    alert(`유저-${id}(${nickname}) 대상을 친구로 추가합니다.`);
-  }, [id, nickname]);
+  const { isSubmitting, error, clearError, cancelRef, onAddFriend } =
+    useAddFriend(id);
 
   return (
-    <HStack
-      fontSize="xl"
-      borderWidth="1px"
-      borderRadius="md"
-      h="45px"
-      w="100%"
-      p={3}
-      justifyContent="space-between"
-    >
-      <Avatar name={nickname} size="sm" src={profileIcon} />
-      <HStack>
-        <Text>{nickname}</Text>
-        <Button backgroundColor="transparent" p={1} onClick={onAddFriend}>
-          <Icon
-            as={FaUserPlus}
-            boxSize="2em"
-            p="0.3em"
-            border="1px"
-            borderRadius="md"
-            borderColor="gray.300"
-          />
-        </Button>
+    <>
+      <HStack
+        fontSize="xl"
+        borderWidth="1px"
+        borderRadius="md"
+        h="45px"
+        w="100%"
+        p={3}
+        justifyContent="space-between"
+      >
+        <Avatar name={nickname} size="sm" src={profileIcon} />
+        <HStack>
+          <Text>{nickname}</Text>
+          <Button
+            backgroundColor="transparent"
+            p={1}
+            onClick={onAddFriend}
+            isLoading={isSubmitting}
+          >
+            <Icon
+              as={FaUserPlus}
+              boxSize="2em"
+              p="0.3em"
+              border="1px"
+              borderRadius="md"
+              borderColor="gray.300"
+            />
+          </Button>
+        </HStack>
       </HStack>
-    </HStack>
+      <WarningAlertDialog
+        isOpen={error.bodyMessage.length > 0}
+        onClose={() => clearError()}
+        cancelRef={cancelRef}
+        headerMessage={error.headerMessage}
+        bodyMessage={error.bodyMessage}
+      />
+    </>
   );
 }
 
