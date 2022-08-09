77ed6a3e516a7cd404fee01dc37d96767c4c9064
Jeongwook Lee <jeongwle@c2r5s7.42seoul.kr>
2022-08-09 17:51:40 +0900

    [TL42-153] fix: 일단 다시 원래대로 되돌림
    문제가 되는부분을 빠른시간안에 찾지 못할 듯 하여
    다른분들의 작업에 영향이 없도록 원래대로 되돌린 후
    다시 찾을 예정.

--- front-end/src/UI/Organisms/UserProfileInfo/index.tsx
+++ front-end/src/UI/Organisms/UserProfileInfo/index.tsx
@@ -8,7 +8,6 @@ import {
   Input,
 } from '@chakra-ui/react';
 import { MdAddPhotoAlternate } from 'react-icons/md';
-import { Field, Form, Formik } from 'formik';
 import ModifiableUserName from '../../Molecules/ModifiableUserName';
 
 function UserProfileInfo(props: {
@@ -18,40 +17,32 @@ function UserProfileInfo(props: {
 }) {
   const { userName, userImage, isMyself } = props;
 
-  //나중에 이미지 저장할 로직 추가
-  const onSubmitHandler = () => {};
-
   return (
     <HStack>
       <Avatar name={userName} src={userImage} size="xl">
         {isMyself && (
           <AvatarBadge borderWidth={0}>
             <AspectRatio width="30px" ratio={1}>
-              <Formik initialValues={{}} onSubmit={onSubmitHandler}>
-                <Box>
-                  <Box
-                    position="relative"
-                    height="100%"
-                    width="100%"
-                    fontSize="3xl"
-                    color="gray"
-                  >
-                    <MdAddPhotoAlternate />
-                  </Box>
-                  <Form>
-                    <Field
-                      as={Input}
-                      type="file"
-                      accept="image/*"
-                      position="absolute"
-                      top="0"
-                      left="0"
-                      opacity="0"
-                      cursor="pointer"
-                    />
-                  </Form>
+              <Box>
+                <Box
+                  position="relative"
+                  height="100%"
+                  width="100%"
+                  fontSize="3xl"
+                  color="gray"
+                >
+                  <MdAddPhotoAlternate />
                 </Box>
-              </Formik>
+                <Input
+                  type="file"
+                  accept="image/*"
+                  position="absolute"
+                  top="0"
+                  left="0"
+                  opacity="0"
+                  cursor="pointer"
+                />
+              </Box>
             </AspectRatio>
           </AvatarBadge>
         )}
