b676789ce68ea80a01d935fe0548a32cc555f8cc
Lee Jihoon <jihoolee@student.42seoul.kr>
2022-08-09 20:20:20 +0900

    [TL42-115] feat: 게임 초대 토스트 훅 구현
    - front-end/src/UI/molecules/GameInvitationContent: 토스트 내부에 들어갈 컴포넌트
    - front-end/Hooks/useInvitation: UseToast훅을 이용한 커스텀 훅
    
    GameInvitationContent에는 useInvitation안의 handleInvitation콜백 함수가 들어가서
    어떤 동작을 할 지 정해줄 수 있음.
    
    각 Toast별로 고유한 ToastId가 필요해서, 게임방의 Id가 고유할 거라고 생각해서
    게임방의 Id를 넣어주는 식으로 구현했음.

---
+++ front-end/src/UI/Molecules/GameInvitationContent/index.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import {
+  Grid,
+  GridItem,
+  Avatar,
+  Text,
+  HStack,
+  IconButton,
+} from '@chakra-ui/react';
+import { CheckIcon, CloseIcon } from '@chakra-ui/icons';
+
+function GameInvitationContent(props: {
+  userName: string;
+  gameId: number;
+  gameMode: string;
+  isRanked: boolean;
+  handleInvitation: (
+    toastId: number,
+    response: boolean,
+    AcceptanceCallback: () => void,
+  ) => void;
+}) {
+  const { userName, gameId, gameMode, isRanked, handleInvitation } = props;
+
+  return (
+    <Grid
+      h="100px"
+      w="100%"
+      bgColor="white"
+      templateRows="repeat(3, 1fr)"
+      templateColumns="repeat(10, 1fr)"
+      borderWidth="1px"
+      borderRadius="md"
+      paddingX={3}
+      paddingY={2}
+    >
+      <GridItem rowSpan={3} colSpan={1} margin="auto">
+        <Avatar name={userName} size="md" />
+      </GridItem>
+      <GridItem rowSpan={1} colSpan={1} />
+      <GridItem rowSpan={2} colSpan={8}>
+        <Text fontSize="lg" lineHeight={1.25}>
+          {userName}님께서
+          <br />
+          {gameMode} {isRanked ? '랭크' : '일반'} 게임에 초대하였습니다.
+        </Text>
+      </GridItem>
+      <GridItem rowSpan={2} colSpan={1} />
+      <GridItem rowSpan={1} colSpan={4} flex="">
+        <Text fontSize="sm" paddingY={1.5}>
+          방금전
+        </Text>
+      </GridItem>
+      <GridItem rowSpan={1} colSpan={2} />
+      <GridItem rowSpan={1} colSpan={2}>
+        <HStack spacing={1}>
+          <IconButton
+            colorScheme="green"
+            aria-label="accept"
+            icon={<CheckIcon />}
+            size="sm"
+            onClick={() => handleInvitation(gameId, true, () => {})}
+          />
+          <IconButton
+            colorScheme="red"
+            aria-label="decline"
+            icon={<CloseIcon />}
+            size="sm"
+            onClick={() => handleInvitation(gameId, false, () => {})}
+          />
+        </HStack>
+      </GridItem>
+    </Grid>
+  );
+}
+
+export default GameInvitationContent;
