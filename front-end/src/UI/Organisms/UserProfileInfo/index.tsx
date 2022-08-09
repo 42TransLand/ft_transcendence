94d73134a442d40b8a5aa2310b5870b50dc97c6d
Jeongwook Lee <jeongwle@c2r5s7.42seoul.kr>
2022-08-09 16:38:34 +0900

    [TL42-153] refactor: Input태그 Formik으로 수정
    제목 그대로 이미지를 받아오는 Input태그를 Formik을
    이용하여 form으로 감싸주는 모양으로 수정. 나중에 이미지를
    저장하는 방식을 정해서 onsubmithandler에 추가해주면 될 것 같다.

--- front-end/src/UI/Organisms/UserProfileInfo/index.tsx
+++ front-end/src/UI/Organisms/UserProfileInfo/index.tsx
@@ -8,6 +8,7 @@ import {
   Input,
 } from '@chakra-ui/react';
 import { MdAddPhotoAlternate } from 'react-icons/md';
+import { Field, Form, Formik } from 'formik';
 import ModifiableUserName from '../../Molecules/ModifiableUserName';
 
 function UserProfileInfo(props: {
@@ -17,32 +18,40 @@ function UserProfileInfo(props: {
 }) {
   const { userName, userImage, isMyself } = props;
 
+  //나중에 이미지 저장할 로직 추가
+  const onSubmitHandler = () => {};
+
   return (
     <HStack>
       <Avatar name={userName} src={userImage} size="xl">
         {isMyself && (
           <AvatarBadge borderWidth={0}>
             <AspectRatio width="30px" ratio={1}>
-              <Box>
-                <Box
-                  position="relative"
-                  height="100%"
-                  width="100%"
-                  fontSize="3xl"
-                  color="gray"
-                >
-                  <MdAddPhotoAlternate />
+              <Formik initialValues={{}} onSubmit={onSubmitHandler}>
+                <Box>
+                  <Box
+                    position="relative"
+                    height="100%"
+                    width="100%"
+                    fontSize="3xl"
+                    color="gray"
+                  >
+                    <MdAddPhotoAlternate />
+                  </Box>
+                  <Form>
+                    <Field
+                      as={Input}
+                      type="file"
+                      accept="image/*"
+                      position="absolute"
+                      top="0"
+                      left="0"
+                      opacity="0"
+                      cursor="pointer"
+                    />
+                  </Form>
                 </Box>
-                <Input
-                  type="file"
-                  accept="image/*"
-                  position="absolute"
-                  top="0"
-                  left="0"
-                  opacity="0"
-                  cursor="pointer"
-                />
-              </Box>
+              </Formik>
             </AspectRatio>
           </AvatarBadge>
         )}
