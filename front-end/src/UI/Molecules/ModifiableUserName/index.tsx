069a20e81cac0c7a74d63d0d2313ff6d6b47bb7d
Jeongwook Lee <jeongwle@c2r5s7.42seoul.kr>
2022-08-10 17:23:04 +0900

    [TL42-142] feat: Input을 form으로 감싸고 닉네임 변경
    닉네임을 Input으로 받는 것을 form으로 감싼 후
    axios.patch를 이용해 닉네임 변경하는 로직 추가
    닉네임이 중복일 경우 alert를 띄우도록 함

--- front-end/src/UI/Molecules/ModifiableUserName/index.tsx
+++ front-end/src/UI/Molecules/ModifiableUserName/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState } from 'react';
 import {
   Text,
   Button,
@@ -6,49 +6,86 @@ import {
   Input,
   InputGroup,
   InputRightElement,
-  FormErrorMessage,
-  FormControl,
 } from '@chakra-ui/react';
+import * as Yup from 'yup';
 import { IoMdSave } from 'react-icons/io';
+import { ErrorMessage, Field, Form, Formik, FormikHelpers } from 'formik';
+import axios from 'axios';
+import WarningAlertDialog from '../../Templates/WarningAlertDialog';
+
+type ChangeNameProps = {
+  nickname: string;
+};
+
+const ChangeNameScheme = Yup.object().shape({
+  nickname: Yup.string().required('닉네임을 입력해주세요.'),
+});
 
 function ModifiableUserName(props: { userName: string; isMyself: boolean }) {
   const { userName, isMyself } = props;
   const [isEditing, setIsEditing] = useState(false);
   const [modUserName, setModUserName] = useState(userName);
-  const isError = useMemo(() => modUserName.length === 0, [modUserName]);
+  const [error, setError] = React.useState({
+    headerMessage: '',
+    bodyMessage: '',
+  });
+  const cancelRef = React.useRef(null);
 
-  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setModUserName(e.target.value);
-  };
+  const onSubmitHandler = React.useCallback(
+    ({ nickname }: ChangeNameProps, helper: FormikHelpers<ChangeNameProps>) => {
+      axios
+        .patch('/users/me', { nickname })
+        .then(() => {
+          setModUserName(nickname);
+          setIsEditing(false);
+        })
+        .catch((err) => {
+          if (err.response.status === 409) {
+            setError({
+              headerMessage: '닉네임 변경 실패',
+              bodyMessage: '중복된 닉네임입니다.',
+            });
+          }
+        });
+      helper.setSubmitting(false);
+    },
+    [setModUserName, setIsEditing],
+  );
 
   if (!isMyself) {
     return <Text fontSize="3xl">{userName}</Text>;
   }
   return isEditing ? (
-    <FormControl isInvalid={isError}>
-      <form
-        onSubmit={(e: React.FormEvent) => {
-          e.preventDefault();
-          if (!isError) setIsEditing(false);
-        }}
-      >
-        <InputGroup>
-          <Input
-            variant="flushed"
-            size="lg"
-            onChange={handleOnChange}
-            value={modUserName}
-            isInvalid={isError}
+    <Formik
+      initialValues={{ nickname: modUserName }}
+      onSubmit={onSubmitHandler}
+      validationSchema={ChangeNameScheme}
+    >
+      {({ isSubmitting }) => (
+        <>
+          <Form>
+            <InputGroup>
+              <Field as={Input} name="nickname" variant="flushed" size="lg" />
+              <InputRightElement>
+                <Button type="submit" isLoading={isSubmitting}>
+                  <Icon as={IoMdSave} boxSize="1.5rem" />
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+            <Text fontSize="xs" textColor="red.500">
+              <ErrorMessage name="nickname" />
+            </Text>
+          </Form>
+          <WarningAlertDialog
+            isOpen={error.bodyMessage.length > 0}
+            onClose={() => setError({ headerMessage: '', bodyMessage: '' })}
+            cancelRef={cancelRef}
+            headerMessage={error.headerMessage}
+            bodyMessage={error.bodyMessage}
           />
-          <InputRightElement>
-            <Input as={Button} type="submit" isInvalid={false}>
-              <Icon as={IoMdSave} boxSize="1.5rem" />
-            </Input>
-          </InputRightElement>
-        </InputGroup>
-        {isError && <FormErrorMessage>빈 칸 안 돼</FormErrorMessage>}
-      </form>
-    </FormControl>
+        </>
+      )}
+    </Formik>
   ) : (
     <Text
       onClick={() => {
