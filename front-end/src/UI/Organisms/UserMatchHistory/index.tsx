b281226710cab00568bde673b65096b8588cbdfe
Jihoon Lee <brain2400@gmail.com>
2022-08-11 15:41:17 +0900

    [TL42-175] style: UserMatch컴포넌트 스타일링

--- front-end/src/UI/Organisms/UserMatchHistory/index.tsx
+++ front-end/src/UI/Organisms/UserMatchHistory/index.tsx
@@ -5,6 +5,7 @@ import ElementList from '../ElementList';
 
 const matchHistories = [
   {
+    matchId: 1,
     opponentUserName: '엄준식',
     opponentUserImage:
       'https://mblogthumb-phinf.pstatic.net/MjAyMDA2MTlfMTY5/MDAxNTkyNTAyNDM2ODcy.FVNsc1SOtS2sUfyaajXNhZpYzAKIFeUg_vCTqzHW4SIg.kQsV680NF1XfoVcDgPg64yF0RzHyRs0-raId3LTIIG4g.JPEG.wndyd75/hqdefault1.jpg?type=w2',
@@ -14,6 +15,7 @@ const matchHistories = [
     isNormalMode: true,
   },
   {
+    matchId: 2,
     opponentUserName: 'PAKA',
     opponentUserImage:
       'https://static-cdn.jtvnw.net/jtv_user_pictures/98bb53c3-4e2f-47f3-9c4b-6c0484b383f6-profile_image-300x300.png',
@@ -23,6 +25,7 @@ const matchHistories = [
     isNormalMode: true,
   },
   {
+    matchId: 3,
     opponentUserName: 'YuriMyWife',
     opponentUserImage:
       'https://w.namu.la/s/cb44538b575f372318ce9c28cd806dde1e6ac146514283b57b696ad7226ca18971fdae5716367e639a3345594b8cdfbf119bcc971b7a686b17c0873402ad659d555a847603c4aa8462a6f783e206b9381a506042def4729ceb696cf6394d4db8',
@@ -39,6 +42,7 @@ function UserMatchHistory() {
       <ElementList>
         {matchHistories.map((m) => (
           <UserMatch
+            key={m.matchId}
             opponentUserName={m.opponentUserName}
             opponentUserImage={m.opponentUserImage}
             userScore={m.userScore}
