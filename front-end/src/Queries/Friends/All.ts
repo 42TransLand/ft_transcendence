0d4d3dd9271c32740970775e488139603011ac8d
Tensiya <40755203+T2SU@users.noreply.github.com>
2022-08-15 10:23:40 +0900

    fix: 친구 수락 후, 목록에 바로 반영되게끔 수정
    - 쿼리는 'friends'로 받아오는데, invalidateQueries는 'friend'로
      받는 문제가 있어서 양측 모두 'friend'로  수정하였음.

--- front-end/src/Queries/Friends/All.ts
+++ front-end/src/Queries/Friends/All.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import UserInfoProps from '../../Props/UserInfoProps';
 import BuildQuery from '../QueryBuilder';
 
-const FRIEND_GET = BuildQuery<UserInfoProps[]>(['friends'], () =>
+const FRIEND_GET = BuildQuery<UserInfoProps[]>(['friend'], () =>
   axios.get('/friend'),
 );
 
