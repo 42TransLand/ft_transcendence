727997b24418d54b6eb886be581b0ef4f57c46bb
Tensiya <40755203+T2SU@users.noreply.github.com>
2022-08-17 20:37:14 +0900

    [TL-XX] fix: 검색창 아이콘 삭제

--- front-end/src/UI/Atoms/SearchBar/index.tsx
+++ front-end/src/UI/Atoms/SearchBar/index.tsx
@@ -1,12 +1,5 @@
 import React from 'react';
-import {
-  Input,
-  InputGroup,
-  InputRightElement,
-  Button,
-  Box,
-} from '@chakra-ui/react';
-import { SearchIcon } from '@chakra-ui/icons';
+import { Input, InputGroup, Box } from '@chakra-ui/react';
 import { Formik, Form, FormikValues } from 'formik';
 
 function SearchBar({
@@ -27,11 +20,6 @@ function SearchBar({
         <Form>
           <InputGroup w="100%" size="2xl">
             <Input size="2xl" onChange={onSubmitHandler} />
-            <InputRightElement>
-              <Button type="submit" size="xs">
-                <SearchIcon />
-              </Button>
-            </InputRightElement>
           </InputGroup>
         </Form>
       </Box>
