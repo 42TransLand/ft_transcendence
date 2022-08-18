b5a557c5efda7dfc219afda9da4fd6f87d254115
Jihoon Lee <brain2400@gmail.com>
2022-08-18 15:08:41 +0900

    [TL42-229] fix: 빠르게 실행시 error fix
    - dependency에 isLoading을 추가하여 정보를 받아오기 전에는 dispatch하지
      않도록 수정
    
    - form입력시 post 요청 및 dispatch하도록 추가만 해둠.

--- front-end/src/UI/Templates/DMContent/index.tsx
+++ front-end/src/UI/Templates/DMContent/index.tsx
@@ -9,7 +9,6 @@ import useFriends from '../../../Hooks/useFriends';
 import USERS_PROFILE_GET from '../../../Queries/Users/Profile';
 import { useQuery } from '@tanstack/react-query';
 import ChatMemberRole from '../../../Props/ChatMemberRole';
-// import DirectMessageProps from '../../../Props/DirectMessageProps';
 
 function dispatchRoomInfo(
   dispatch: (value: ChatActionType) => void,
@@ -55,69 +54,9 @@ function DMContent() {
   const targetName: string = userName ?? '';
   const [, dispatch] = useChat();
   const { id: myId, nickname: myName, profileImg: myProfileImg } = useMe();
-  const { data } = useQuery(USERS_PROFILE_GET(targetName));
+  const { isLoading, data } = useQuery(USERS_PROFILE_GET(targetName));
 
   useEffect(() => {
-    dispatchRoomInfo(
-      dispatch,
-      'private',
-      data?.id ? Number(data?.id) : 0,
-      targetName,
-    );
-    dispatchRoomMember(
-      dispatch,
-      myId,
-      myName,
-      `${process.env.REACT_APP_API_HOST}/${myProfileImg}`,
-      ChatMemberRole.MEMBER,
-      false,
-      false,
-    );
-    dispatchRoomMember(
-      dispatch,
-      data?.id ?? '',
-      data?.nickname ?? '',
-      `${process.env.REACT_APP_API_HOST}/${data?.profileImg}`,
-      ChatMemberRole.MEMBER,
-      false,
-      false,
-    );
-    dispatch({
-      action: 'chat',
-      name: '엄준식은살아있다',
-      message: '왜',
-    });
-    // dispatch({
-    //   action: 'updateInfo',
-    //   chatInfo: {
-    //     roomType: 'private',
-    //     channelId: 0, // dummy
-    //     channelName: targetName,
-    //     maxHeadCount: 2,
-    //   },
-    // });
-    // dispatch({
-    //   action: 'insertMember',
-    //   chatMember: {
-    //     userId: myId,
-    //     name: myName,
-    //     profileImg: `${process.env.REACT_APP_API_HOST}/${myProfileImg}`,
-    //     role: ChatMemberRole.MEMBER,
-    //     muted: false,
-    //     blocked: false,
-    //   },
-    // });
-    // dispatch({
-    //   action: 'insertMember',
-    //   chatMember: {
-    //     userId: data?.id ?? '',
-    //     name: data?.nickname ?? '',
-    //     profileImg: `${process.env.REACT_APP_API_HOST}/${data?.profileImg}`,
-    //     role: ChatMemberRole.MEMBER,
-    //     muted: false,
-    //     blocked: false,
-    //   },
-    // });
     axios
       .get(`/dms/${targetName}`)
       .then((response) => {
@@ -126,7 +65,35 @@ function DMContent() {
       .catch((err) => {
         console.log('err:', err);
       });
-  }, [targetName, dispatch]);
+  }, []);
+  useEffect(() => {
+    if (data) {
+      dispatchRoomInfo(
+        dispatch,
+        'private',
+        data?.id ? Number(data?.id) : 0,
+        targetName,
+      );
+      dispatchRoomMember(
+        dispatch,
+        myId,
+        myName,
+        `${process.env.REACT_APP_API_HOST}/${myProfileImg}`,
+        ChatMemberRole.MEMBER,
+        false,
+        false,
+      );
+      dispatchRoomMember(
+        dispatch,
+        data?.id ?? '',
+        data?.nickname ?? '',
+        `${process.env.REACT_APP_API_HOST}/${data?.profileImg}`,
+        ChatMemberRole.MEMBER,
+        false,
+        false,
+      );
+    }
+  }, [targetName, dispatch, isLoading]);
   return <div />;
 }
 
