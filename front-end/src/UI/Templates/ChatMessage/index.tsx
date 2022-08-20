a2a357555224563b8f24ae5f2a8d4936ba86785f
Jeongwook Lee <jeongwle@c2r5s7.42seoul.kr>
2022-08-20 19:14:43 +0900

    [TL42-238] feat: 입장, 퇴장, 채팅 미완
    RouteModal에서 onclose 일 경우 채팅방을 나간다고 백엔드에
    신호 주도록 변경, 그 외에 입장, 채팅을 삽질하면서 구현해 보았지만
    혼자서 해결하지 못한 문제들이 남음. 채팅방 입장한 유저를 2번 그리는 것..
    도와주세요

--- front-end/src/UI/Templates/ChatMessage/index.tsx
+++ front-end/src/UI/Templates/ChatMessage/index.tsx
@@ -1,6 +1,5 @@
-/* eslint-disable*/
 import React, { useCallback, useEffect } from 'react';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { useNavigate, useParams } from 'react-router-dom';
 import { FormikHelpers } from 'formik';
 import axios from 'axios';
@@ -10,9 +9,14 @@ import { useSocket } from '../../../Hooks/useSocket';
 import useWarningDialog from '../../../Hooks/useWarningDialog';
 import CHAT_USERS_GET from '../../../Queries/ChatUsers/All';
 import ChatModal from '../../Organisms/ChatModal';
+import SocketEventName from '../../../WebSockets/dto/constants/socket.events.enum';
+import ChatJoinNotifyProps from '../../../WebSockets/dto/res/chat.join.notify.dto';
+import ChatMemberRole from '../../../Props/ChatMemberRole';
+import ChatLeaveNotifyProps from '../../../WebSockets/dto/res/chat.leave.notify.dto';
+import ChatMessageProps from '../../../WebSockets/dto/res/chat.message.notify.dto';
 
 export default function ChatMessage() {
-  const { dispatchRoomInfo, dispatchChat, displayDMHistory, insertRoomMember } =
+  const { dispatchRoomInfo, dispatchChat, insertRoomMember, deleteRoomMember } =
     useMessage();
   const navigate = useNavigate();
   const { nickname } = useMe();
@@ -23,6 +27,7 @@ export default function ChatMessage() {
     navigate(-1),
   );
   const { state } = useSocket();
+  const queryClient = useQueryClient();
 
   const onSubmitHandler = useCallback(
     (
@@ -54,6 +59,26 @@ export default function ChatMessage() {
     [chatRoomId, dispatchChat, nickname, setError],
   );
 
+  const leaveChatRoomHandler = () => {
+    if (chatRoomId === '') return;
+    axios
+      .delete(`/chat/leave/${chatRoomId}`)
+      .then(() => queryClient.invalidateQueries(['channels']))
+      .catch((err) => {
+        if (err.response) {
+          setError({
+            headerMessage: '오류 발생',
+            bodyMessage: err.response.data.message,
+          });
+        } else {
+          setError({
+            headerMessage: '오류 발생',
+            bodyMessage: err.message,
+          });
+        }
+      });
+  };
+
   useEffect(() => {
     if (isLoading) return;
     if (!chatRoomId || error) {
@@ -63,21 +88,71 @@ export default function ChatMessage() {
           ? '서버와의 통신에 실패했습니다.'
           : '정상적인 접근이 아닙니다.',
       });
-      return;
     }
-    // axios.post();
-    // state.socket?.on(
-    //   SocketEventName.CHAT_MESSAGE_NOTIFY,
-    //   (dto: ChatMessageProps) => {
-    //     if (dto.chatRoomId === chatRoomId) {
-    //       dispatchChat(dto.nickname, dto.content);
-    //     }
-    //   },
-    // );
-  });
+  }, [chatRoomId, error, isLoading, setError]);
+
+  /** 채팅방에 입장할 경우 get으로 받아와서 한번 유저를 그리고 그 다음 소켓으로 온 정보로 또 그려서 2명이 그려지는 현상을 어떻게 해결해야 할 지 모르겠습니다. */
+  useEffect(() => {
+    data
+      ?.filter((m) => m.user.nickname !== nickname)
+      .forEach((member) => {
+        insertRoomMember({
+          userId: member.user.id,
+          name: member.user.nickname,
+          profileImg: `${process.env.REACT_APP_API_HOST}/${member.user.profileImg}`,
+          role:
+            member.role === 'OWNER'
+              ? ChatMemberRole.OWNER
+              : member.role === 'ADMIN'
+              ? ChatMemberRole.ADMIN
+              : ChatMemberRole.MEMBER,
+          muted: false,
+          blocked: false,
+        });
+      });
+  }, [data, insertRoomMember]);
+
+  useEffect(() => {
+    state.socket?.on(
+      SocketEventName.CHAT_JOIN_NOTIFY,
+      (dto: ChatJoinNotifyProps) => {
+        insertRoomMember({
+          profileImg: `${process.env.REACT_APP_API_HOST}/${dto.profileImg}`,
+          userId: dto.id,
+          name: dto.nickname,
+          role: ChatMemberRole.MEMBER,
+          muted: false,
+          blocked: false,
+        });
+      },
+    );
+    state.socket?.on(
+      SocketEventName.CHAT_LEAVE_NOTIFY,
+      (dto: ChatLeaveNotifyProps) => {
+        deleteRoomMember(dto.nickname);
+      },
+    );
+    state.socket?.on(
+      SocketEventName.CHAT_MESSAGE_NOTIFY,
+      (dto: ChatMessageProps) => {
+        if (dto.nickname !== nickname) {
+          dispatchChat(dto.nickname, dto.content);
+        }
+      },
+    );
+    return () => {
+      state.socket?.off(SocketEventName.CHAT_JOIN_NOTIFY);
+      state.socket?.off(SocketEventName.CHAT_LEAVE_NOTIFY);
+      state.socket?.off(SocketEventName.CHAT_MESSAGE_NOTIFY);
+    };
+  }, [state.socket, insertRoomMember, deleteRoomMember, dispatchChat]);
+
   return (
     <>
-      <ChatModal onSubmitHandler={onSubmitHandler} />
+      <ChatModal
+        onSubmitHandler={onSubmitHandler}
+        leaveChatRoomHandler={leaveChatRoomHandler}
+      />
       {WarningDialogComponent}
     </>
   );
