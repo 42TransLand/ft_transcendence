5def3360eb0aa70847e1bc557fec705f6a9fb458
Lee-Byung-Kyu <leebang54@khu.ac.kr>
2022-08-23 16:01:58 +0900

    [Tl42-216] feat: 관리자 기능 미완성이지만 푸쉬

--- front-end/src/UI/Molecules/AdminApproveMenu/index.tsx
+++ front-end/src/UI/Molecules/AdminApproveMenu/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Icon } from '@chakra-ui/icons';
 import { IconType } from 'react-icons';
-
 import { MenuItem, Text } from '@chakra-ui/react';
-import { useChat } from '../../../Hooks/useChat';
+// import { useChat } from '../../../Hooks/useChat';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 import { useTargetUser } from '../../../Hooks/useTargetUser';
+import useWarningDialog from '../../../Hooks/useWarningDialog';
 
 function AdminApproveMenu({
   icon,
@@ -15,26 +17,50 @@ function AdminApproveMenu({
   label: string;
   cast: boolean;
 }) {
-  const [, dispatch] = useChat();
+  // const [, dispatch] = useChat();
   const { userName } = useTargetUser();
+  const { id } = useParams();
+  const { setError, WarningDialogComponent } = useWarningDialog();
 
   const handleMenuClick = React.useCallback(() => {
-    dispatch({
-      action: 'enqueueEvent',
-      event: {
-        type: cast ? 'adminApproved' : 'adminUnapproved',
-        target: userName,
-        commandSuccessful: [true, false, undefined][
-          Math.floor(Math.random() * 3)
-        ],
-      },
-    });
-  }, [cast, userName, dispatch]);
+    if (cast) {
+      axios
+        .patch(`/chat/role/${id}`, { userName })
+        .then(() => {
+          // dispatch({
+          //   action: 'enqueueEvent',
+          //   event: {
+          //     type: cast ? 'adminApproved' : 'adminUnapproved',
+          //     target: userName,
+          //     commandSuccessful: [true, false, undefined][
+          //       Math.floor(Math.random() * 3)
+          //     ],
+          //   },
+          // });
+        })
+        .catch((err) => {
+          if (err.response) {
+            setError({
+              headerMessage: 'admin setting error',
+              bodyMessage: err.response.data.message,
+            });
+          } else {
+            setError({
+              headerMessage: 'admin setting error',
+              bodyMessage: err.message,
+            });
+          }
+        });
+    }
+  }, [id, userName, setError, cast]);
 
   return (
-    <MenuItem onClick={handleMenuClick} icon={<Icon as={icon} />}>
-      <Text>{label}</Text>
-    </MenuItem>
+    <>
+      <MenuItem onClick={handleMenuClick} icon={<Icon as={icon} />}>
+        <Text>{label}</Text>
+      </MenuItem>
+      {WarningDialogComponent}
+    </>
   );
 }
 
