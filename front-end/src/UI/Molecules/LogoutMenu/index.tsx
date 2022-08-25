3088e2b25b094f668e63746e32f1bf975fe0d673
Tensiya <40755203+T2SU@users.noreply.github.com>
2022-08-25 15:51:54 +0900

    [TL42-XX] fix: 로그아웃 관련 문제 수정

--- front-end/src/UI/Molecules/LogoutMenu/index.tsx
+++ front-end/src/UI/Molecules/LogoutMenu/index.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
 import { GoSignOut } from 'react-icons/go';
 import { MenuItem } from '@chakra-ui/react';
-import { useNavigate } from 'react-router-dom';
 import { useLogout } from '../../../Hooks/useLogout';
-import { SocketState, useSocket } from '../../../Hooks/useSocket';
 
 export default function LogoutMenu() {
-  const navigate = useNavigate();
   const logout = useLogout();
-  const { state, dispatch } = useSocket();
-  React.useEffect(() => {
-    if (state.socketState === SocketState.DISCONNECTED) {
-      logout();
-      navigate('/', { replace: true });
-    }
-  }, [state.socketState, logout, navigate]);
-  const handleMenuClick = React.useCallback(() => {
-    dispatch({ action: 'disconnect' });
-  }, [dispatch]);
 
   return (
-    <MenuItem onClick={handleMenuClick} icon={<GoSignOut />}>
+    <MenuItem onClick={logout} icon={<GoSignOut />}>
       로그아웃
     </MenuItem>
   );
