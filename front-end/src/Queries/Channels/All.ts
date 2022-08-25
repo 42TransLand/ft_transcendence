e8e632838a49cba4dec30baa5af9ca5296310adf
Lee-Byung-Kyu <leebang54@khu.ac.kr>
2022-08-25 13:57:17 +0900

    [TL42-XX] fix: 실시간으로 친구 알림 채널 갱신

--- front-end/src/Queries/Channels/All.ts
+++ front-end/src/Queries/Channels/All.ts
@@ -2,8 +2,10 @@ import axios from 'axios';
 import ChannelProps from '../../Props/ChannelProps';
 import BuildQuery from '../QueryBuilder';
 
-const CHANNEL_GET = BuildQuery<ChannelProps[]>(['channels'], () =>
-  axios.get('/chat'),
+const CHANNEL_GET = BuildQuery<ChannelProps[]>(
+  ['channels'],
+  () => axios.get('/chat'),
+  { refetchInterval: 1000 },
 );
 
 export default CHANNEL_GET;
--- front-end/src/Queries/Friends/All.ts
+++ front-end/src/Queries/Friends/All.ts
@@ -2,8 +2,10 @@ import axios from 'axios';
 import FriendInfoProps from '../../Props/FriendInfoProps';
 import BuildQuery from '../QueryBuilder';
 
-const FRIEND_GET = BuildQuery<FriendInfoProps[]>(['friend'], () =>
-  axios.get('/friend'),
+const FRIEND_GET = BuildQuery<FriendInfoProps[]>(
+  ['friend'],
+  () => axios.get('/friend'),
+  { refetchInterval: 1000 },
 );
 
 export default FRIEND_GET;
